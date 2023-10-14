1f20244ec302d2523bd1821ebb32f19009c087e5
Werner van der Merwe <wvdm1217@gmail.com>
2023-10-14 12:10:07 +0200

    fix: sidebar link to the creation post

--- blog/.vitepress/config.ts
+++ blog/.vitepress/config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         text: 'Blog',
         items: [
           { text: 'Introduction', link: '/blog' },
-          { text: 'The Creation', link: 'posts/creation' }
+          { text: 'The Creation', link: '/posts/2023-09-08-creation' }
         ]
       }      
     ],
